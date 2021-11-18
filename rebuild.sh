f827f7246519466be2f9209d86183ff33f1ad7ca
123nishith <89917184+123nishith@users.noreply.github.com>
2021-11-18 09:15:10 +0530

    Add files via upload

---
+++ rebuild.sh
@@ -0,0 +1,5 @@
+set -e
+cd "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )\sm64plus" || { tput setaf 1; echo "Couldn't enter the repository directory."; read -n1 -r -p "Press any key to close this window." key; }
+git pull --ff-only || { tput setaf 3; echo "Pulling the game files from the repository failed. This is normal if you have modified the files."; }
+tput setaf 6
+make -j$(nproc) || { tput setaf 1; echo "An error has occured during the building process."; read -n1 -r -p "Press any key to close this window." key; }
